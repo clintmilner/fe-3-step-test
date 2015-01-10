2e2ff1a289f7ed0143607a634c0af08b7e50a3a9
Clint Milner <clint@sympoz.com>
2015-01-10 22:41:06 +0000

    cleaned up code and finished documentation of test_3

--- js/test_3.js
+++ js/test_3.js
@@ -1,59 +1,101 @@
-function w( e, n, r )
-{
-    if ( e.attr( "data-type" ) == "warning" )
-    {
-        ei = "warning"
-    }
-    else if ( e.attr( "data-type" ) == "error" )
-    {
-        ei = "error"
-    }
-    else
-    {
-        ei = "info"
-    }
-    id = "tt" + Math.floor( Math.random() * 1e4 );
-    $u = e.attr( "data-u" ).split( "<br/>" );
-    var tooltipMeta = {id: id, type: ei, u: $u};
-    var tooltip = tmplEngine.render( tmpl, tooltipMeta );
-    $( "body" ).append( tooltip );
-    $( "#" + id ).css( {top: n, left: r} ).hide().stop( true, false ).fadeIn( 200 )
-}
-function q()
-{
-    $( "#" + id ).stop( true, false ).fadeOut( 200, function ()
-    {
-        $( this ).remove()
-    } )
-}
-tmplEngine = Mustache;
-var r = {};
-var tmpl = '<div class="nvTt" id="{{id}}"><span class="nvTt--corner nvTt--{{type}}"></span><p class="nvTt--u">{{#u}}{{.}}<br/>{{/u}}</p></div>';
-var ei = "", $u = "", id = "";
-$( document ).on( "mouseover mouseout mousedown", ".e", function ( e )
-{
-    var $this = $( this );
-    if ( e.type == "mouseover" )
-    {
-        var n = e.pageY + 10,
-            r = e.pageX + 10;
+/**
+ * nVidia ToolTip Widget
+ *
+ * Usage: Any element with a class of 'tooltip' will have a listener pointed to this code.
+ *        Elements should have a data attribute of 'msg' which is the text of the tooltip,
+ *        and a data attribute of 'type' which determine the style of tooltip.
+ *
+ * Dependencies: jQuery, Template.js
+ *
+ * Markup Example: <div class="tooltip" data-msg="Here is text for a tooltip" data-type="info">Lorem Ipsum</div>
+ */
 
-        w( $this, n, r )
-    }
-    else
+(function(){
+
+    var tmplEngine = Mustache,
+        tmpl =
+            '<div class="nvTt" id="{{id}}">' +
+                '<span class="nvTt--corner nvTt--{{type}}"></span>' +
+                '<p class="nvTt--u">{{#msg}}{{.}}<br/>{{/msg}}</p>' +
+            '</div>',
+        type, id;
+
+    function buildTooltip( elem, posY, posX )
     {
-        q()
+        var tooltipMeta, output, msg;
+
+        // discover which type of tooltip to show
+        if ( elem.attr( "data-type" ) === "warning" )
+        {
+            type = "warning"
+        }
+        else if ( elem.attr( "data-type" ) === "error" )
+        {
+            type = "error"
+        }
+        else
+        {
+            type = "info"
+        }
+
+        // create randomised id for tooltip element
+        id = "tt" + Math.floor( Math.random() * 1e4 );
+
+        // assign the tooltip to the msg variable
+        msg = elem.attr( "data-msg" ).split( "<br/>" );
+
+        // build up tooltip object for templating
+        tooltipMeta = {id: id, type: type, msg: msg};
+
+        // use Moustache to render tooltip,
+        // add it to the DOM,
+        // and position the tooltip with CSS
+        // before fading it in
+        output = tmplEngine.render( tmpl, tooltipMeta );
+        $( "body" ).append( output );
+        $( "#" + id ).css(
+            {
+                top: posY,
+                left: posX
+            })
+            .hide()
+            .stop( true, false )
+            .fadeIn( 200 );
     }
-    $this.on( "remove", function ( e )
+    function removeTooltip()
     {
-        q()
-    } )
-} );
-r.add = function ( e, tooltip, n )
-{
-    e.addClass( "e" ).attr( {"data-u": tooltip, "data-type": n || ""} )
-};
-r.destroy = function ( e )
-{
-    e.removeClass( "e" )
-};
+        // remove the tooltip; fading it out before
+        // removing it from the DOM
+        $( "#" + id ).stop( true, false )
+            .fadeOut( 200,
+            function ()
+            {
+                $( this ).remove();
+            });
+    }
+
+    $( document ).on( "mouseover mouseout mousedown", ".tooltip", function ( event )
+    {
+        // Event Listener for elements with a class of 'tooltip'
+        // if we're over the element, build up a tooltip,
+        // but if not, make sure it's removed
+
+        var $this = $( this );
+        if ( event.type === "mouseover" )
+        {
+            var posY = event.pageY + 10,
+                posX = event.pageX + 10;
+
+            buildTooltip( $this, posY, posX );
+        }
+        else
+        {
+            removeTooltip();
+        }
+
+        $this.on( "remove", function ()
+        {
+            removeTooltip();
+        });
+    });
+})();
